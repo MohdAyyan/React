ac56f300b7baf3899a7c647d63661c7f49661da1
MohdAyyan <mohdayyan297@gmail.com>
2024-08-22 21:46:36 +0530

    This folder contains some or imp concepts of react-router

---
+++ 06reactRouter/src/main.jsx
@@ -0,0 +1,66 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider,Route } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import { About, Contact, Github, Home,User,githubInfoLoader } from './components/index.js'
+
+
+//1st Method to create router
+// const router = createBrowserRouter([
+// {
+//   path:"/",
+//   element:<Layout/>,
+//   children:[
+//   {
+//     path:"",
+//     element:<Home/>
+//   },
+
+// {
+//   path:"about",
+//   element:<About/>
+// },
+// {
+//   path:"contact",
+//   element:<Contact/>
+// },
+// // {},
+
+//   ]
+// }
+
+// ])
+
+//2nd Method to create a router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route
+      path='' element={<Home />}
+      />
+       <Route
+      path='about' element={<About />}
+      />
+       <Route
+      path='contact' element={<Contact />}
+      />
+       <Route
+      path='user/:userid' element={<User />}
+      />
+       <Route
+        loader={githubInfoLoader}
+        path='github' 
+        element={<Github />}
+      />
+    </Route>
+  )
+)
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <RouterProvider router={router}/>
+  </StrictMode>,
+)
